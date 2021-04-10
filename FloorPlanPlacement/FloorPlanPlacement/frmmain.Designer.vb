3d64e605468f5396db4928681effcc61191756bc
Boon Khai Ng <ngbk1993@yahoo.com>
2021-04-10 17:45:54 +0800

    Initial Commit: Design Automation Floor Planing
    Signed-off-by: Boon Khai Ng <ngbk1993@yahoo.com>

---
+++ FloorPlanPlacement/FloorPlanPlacement/frmmain.Designer.vb
@@ -0,0 +1,272 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmmain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.picFloorPlan = New System.Windows.Forms.PictureBox()
+        Me.lstLoad = New System.Windows.Forms.ListBox()
+        Me.lstNodeWidth = New System.Windows.Forms.ListBox()
+        Me.lstNodeHeight = New System.Windows.Forms.ListBox()
+        Me.lstNodeName = New System.Windows.Forms.ListBox()
+        Me.cmdLoad = New System.Windows.Forms.Button()
+        Me.cmdPlacement = New System.Windows.Forms.Button()
+        Me.lstNodeNameSort = New System.Windows.Forms.ListBox()
+        Me.lstNodeHeightSort = New System.Windows.Forms.ListBox()
+        Me.lstNodeWidthSort = New System.Windows.Forms.ListBox()
+        Me.lstcordX = New System.Windows.Forms.ListBox()
+        Me.lstcordY = New System.Windows.Forms.ListBox()
+        Me.cmdlg = New System.Windows.Forms.OpenFileDialog()
+        Me.lstlog = New System.Windows.Forms.ListBox()
+        Me.cmdRerun = New System.Windows.Forms.Button()
+        Me.cboScale = New System.Windows.Forms.ComboBox()
+        Me.txtLog = New System.Windows.Forms.TextBox()
+        Me.cmdExport = New System.Windows.Forms.Button()
+        Me.cmOpendlg = New System.Windows.Forms.SaveFileDialog()
+        Me.lstUnscaledNodeWidth = New System.Windows.Forms.ListBox()
+        Me.lstUnscaledNodeHeight = New System.Windows.Forms.ListBox()
+        CType(Me.picFloorPlan, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'picFloorPlan
+        '
+        Me.picFloorPlan.Location = New System.Drawing.Point(12, 12)
+        Me.picFloorPlan.Name = "picFloorPlan"
+        Me.picFloorPlan.Size = New System.Drawing.Size(811, 429)
+        Me.picFloorPlan.TabIndex = 0
+        Me.picFloorPlan.TabStop = False
+        '
+        'lstLoad
+        '
+        Me.lstLoad.FormattingEnabled = True
+        Me.lstLoad.Location = New System.Drawing.Point(284, 638)
+        Me.lstLoad.Name = "lstLoad"
+        Me.lstLoad.Size = New System.Drawing.Size(330, 95)
+        Me.lstLoad.TabIndex = 1
+        Me.lstLoad.Visible = False
+        '
+        'lstNodeWidth
+        '
+        Me.lstNodeWidth.FormattingEnabled = True
+        Me.lstNodeWidth.Location = New System.Drawing.Point(99, 537)
+        Me.lstNodeWidth.Name = "lstNodeWidth"
+        Me.lstNodeWidth.Size = New System.Drawing.Size(78, 95)
+        Me.lstNodeWidth.TabIndex = 2
+        Me.lstNodeWidth.Visible = False
+        '
+        'lstNodeHeight
+        '
+        Me.lstNodeHeight.FormattingEnabled = True
+        Me.lstNodeHeight.Location = New System.Drawing.Point(183, 537)
+        Me.lstNodeHeight.Name = "lstNodeHeight"
+        Me.lstNodeHeight.Size = New System.Drawing.Size(78, 95)
+        Me.lstNodeHeight.TabIndex = 3
+        Me.lstNodeHeight.Visible = False
+        '
+        'lstNodeName
+        '
+        Me.lstNodeName.FormattingEnabled = True
+        Me.lstNodeName.Location = New System.Drawing.Point(15, 537)
+        Me.lstNodeName.Name = "lstNodeName"
+        Me.lstNodeName.Size = New System.Drawing.Size(78, 95)
+        Me.lstNodeName.TabIndex = 4
+        Me.lstNodeName.Visible = False
+        '
+        'cmdLoad
+        '
+        Me.cmdLoad.Location = New System.Drawing.Point(748, 475)
+        Me.cmdLoad.Name = "cmdLoad"
+        Me.cmdLoad.Size = New System.Drawing.Size(75, 23)
+        Me.cmdLoad.TabIndex = 5
+        Me.cmdLoad.Text = "Load"
+        Me.cmdLoad.UseVisualStyleBackColor = True
+        '
+        'cmdPlacement
+        '
+        Me.cmdPlacement.Location = New System.Drawing.Point(748, 521)
+        Me.cmdPlacement.Name = "cmdPlacement"
+        Me.cmdPlacement.Size = New System.Drawing.Size(75, 23)
+        Me.cmdPlacement.TabIndex = 6
+        Me.cmdPlacement.Text = "Placement"
+        Me.cmdPlacement.UseVisualStyleBackColor = True
+        '
+        'lstNodeNameSort
+        '
+        Me.lstNodeNameSort.FormattingEnabled = True
+        Me.lstNodeNameSort.Location = New System.Drawing.Point(15, 638)
+        Me.lstNodeNameSort.Name = "lstNodeNameSort"
+        Me.lstNodeNameSort.Size = New System.Drawing.Size(78, 95)
+        Me.lstNodeNameSort.TabIndex = 9
+        Me.lstNodeNameSort.Visible = False
+        '
+        'lstNodeHeightSort
+        '
+        Me.lstNodeHeightSort.FormattingEnabled = True
+        Me.lstNodeHeightSort.Location = New System.Drawing.Point(183, 638)
+        Me.lstNodeHeightSort.Name = "lstNodeHeightSort"
+        Me.lstNodeHeightSort.Size = New System.Drawing.Size(78, 95)
+        Me.lstNodeHeightSort.TabIndex = 8
+        Me.lstNodeHeightSort.Visible = False
+        '
+        'lstNodeWidthSort
+        '
+        Me.lstNodeWidthSort.FormattingEnabled = True
+        Me.lstNodeWidthSort.Location = New System.Drawing.Point(99, 638)
+        Me.lstNodeWidthSort.Name = "lstNodeWidthSort"
+        Me.lstNodeWidthSort.Size = New System.Drawing.Size(78, 95)
+        Me.lstNodeWidthSort.TabIndex = 7
+        Me.lstNodeWidthSort.Visible = False
+        '
+        'lstcordX
+        '
+        Me.lstcordX.FormattingEnabled = True
+        Me.lstcordX.Location = New System.Drawing.Point(623, 449)
+        Me.lstcordX.Name = "lstcordX"
+        Me.lstcordX.Size = New System.Drawing.Size(78, 95)
+        Me.lstcordX.TabIndex = 10
+        Me.lstcordX.Visible = False
+        '
+        'lstcordY
+        '
+        Me.lstcordY.FormattingEnabled = True
+        Me.lstcordY.Location = New System.Drawing.Point(539, 449)
+        Me.lstcordY.Name = "lstcordY"
+        Me.lstcordY.Size = New System.Drawing.Size(78, 95)
+        Me.lstcordY.TabIndex = 11
+        Me.lstcordY.Visible = False
+        '
+        'lstlog
+        '
+        Me.lstlog.FormattingEnabled = True
+        Me.lstlog.Location = New System.Drawing.Point(15, 447)
+        Me.lstlog.Name = "lstlog"
+        Me.lstlog.Size = New System.Drawing.Size(706, 199)
+        Me.lstlog.TabIndex = 12
+        '
+        'cmdRerun
+        '
+        Me.cmdRerun.Location = New System.Drawing.Point(748, 550)
+        Me.cmdRerun.Name = "cmdRerun"
+        Me.cmdRerun.Size = New System.Drawing.Size(75, 23)
+        Me.cmdRerun.TabIndex = 13
+        Me.cmdRerun.Text = "ReRunFor"
+        Me.cmdRerun.UseVisualStyleBackColor = True
+        '
+        'cboScale
+        '
+        Me.cboScale.FormattingEnabled = True
+        Me.cboScale.Location = New System.Drawing.Point(748, 448)
+        Me.cboScale.Name = "cboScale"
+        Me.cboScale.Size = New System.Drawing.Size(75, 21)
+        Me.cboScale.TabIndex = 14
+        Me.cboScale.Text = "30"
+        '
+        'txtLog
+        '
+        Me.txtLog.Location = New System.Drawing.Point(15, 448)
+        Me.txtLog.Multiline = True
+        Me.txtLog.Name = "txtLog"
+        Me.txtLog.Size = New System.Drawing.Size(706, 197)
+        Me.txtLog.TabIndex = 15
+        '
+        'cmdExport
+        '
+        Me.cmdExport.Location = New System.Drawing.Point(748, 595)
+        Me.cmdExport.Name = "cmdExport"
+        Me.cmdExport.Size = New System.Drawing.Size(74, 25)
+        Me.cmdExport.TabIndex = 16
+        Me.cmdExport.Text = "Export"
+        Me.cmdExport.UseVisualStyleBackColor = True
+        '
+        'lstUnscaledNodeWidth
+        '
+        Me.lstUnscaledNodeWidth.FormattingEnabled = True
+        Me.lstUnscaledNodeWidth.Location = New System.Drawing.Point(351, 537)
+        Me.lstUnscaledNodeWidth.Name = "lstUnscaledNodeWidth"
+        Me.lstUnscaledNodeWidth.Size = New System.Drawing.Size(78, 95)
+        Me.lstUnscaledNodeWidth.TabIndex = 18
+        Me.lstUnscaledNodeWidth.Visible = False
+        '
+        'lstUnscaledNodeHeight
+        '
+        Me.lstUnscaledNodeHeight.FormattingEnabled = True
+        Me.lstUnscaledNodeHeight.Location = New System.Drawing.Point(267, 537)
+        Me.lstUnscaledNodeHeight.Name = "lstUnscaledNodeHeight"
+        Me.lstUnscaledNodeHeight.Size = New System.Drawing.Size(78, 95)
+        Me.lstUnscaledNodeHeight.TabIndex = 17
+        Me.lstUnscaledNodeHeight.Visible = False
+        '
+        'frmmain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(835, 657)
+        Me.Controls.Add(Me.lstUnscaledNodeWidth)
+        Me.Controls.Add(Me.lstUnscaledNodeHeight)
+        Me.Controls.Add(Me.cmdExport)
+        Me.Controls.Add(Me.txtLog)
+        Me.Controls.Add(Me.cboScale)
+        Me.Controls.Add(Me.cmdRerun)
+        Me.Controls.Add(Me.lstLoad)
+        Me.Controls.Add(Me.lstcordY)
+        Me.Controls.Add(Me.lstcordX)
+        Me.Controls.Add(Me.lstNodeNameSort)
+        Me.Controls.Add(Me.lstNodeHeightSort)
+        Me.Controls.Add(Me.lstNodeWidthSort)
+        Me.Controls.Add(Me.cmdPlacement)
+        Me.Controls.Add(Me.cmdLoad)
+        Me.Controls.Add(Me.lstNodeName)
+        Me.Controls.Add(Me.lstNodeHeight)
+        Me.Controls.Add(Me.lstNodeWidth)
+        Me.Controls.Add(Me.picFloorPlan)
+        Me.Controls.Add(Me.lstlog)
+        Me.MaximizeBox = False
+        Me.Name = "frmmain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Floor Plan Placement"
+        CType(Me.picFloorPlan, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents picFloorPlan As PictureBox
+    Friend WithEvents lstLoad As ListBox
+    Friend WithEvents lstNodeWidth As ListBox
+    Friend WithEvents lstNodeHeight As ListBox
+    Friend WithEvents lstNodeName As ListBox
+    Friend WithEvents cmdLoad As Button
+    Friend WithEvents cmdPlacement As Button
+    Friend WithEvents lstNodeNameSort As ListBox
+    Friend WithEvents lstNodeHeightSort As ListBox
+    Friend WithEvents lstNodeWidthSort As ListBox
+    Friend WithEvents lstcordX As ListBox
+    Friend WithEvents lstcordY As ListBox
+    Friend WithEvents cmdlg As OpenFileDialog
+    Friend WithEvents lstlog As ListBox
+    Friend WithEvents cmdRerun As Button
+    Friend WithEvents cboScale As ComboBox
+    Friend WithEvents txtLog As TextBox
+    Friend WithEvents cmdExport As Button
+    Friend WithEvents cmOpendlg As SaveFileDialog
+    Friend WithEvents lstUnscaledNodeWidth As ListBox
+    Friend WithEvents lstUnscaledNodeHeight As ListBox
+End Class
---
+++ FloorPlanPlacement/FloorPlanPlacement/frmmain.vb
@@ -0,0 +1,311 @@
+﻿
+Public Class frmmain
+
+
+    Public Scales As Double = 50
+    Public totalWidth As Double
+    Public totalHeight As Double
+    Public FirstimeRun As Boolean = True
+    Public tArea As Double = 0
+    Public InitialArea As Double = 0
+    Private Function DesignRuleCheck(AtotalHeight As Double, AtotalWidth As Double) As Boolean
+        Dim Pass As Boolean
+        Pass = True
+
+        If AtotalHeight = 0 Or AtotalWidth = 0 Then Return False : Exit Function
+        If AtotalHeight / AtotalWidth > 1.2 Then Pass = False
+        If AtotalWidth / AtotalHeight > 1.2 Then Pass = False
+        DesignRuleCheck = Pass
+    End Function
+    Private Sub Placement(stopWidth As Double)
+        FirstimeRun = False
+        'make all longer to height
+        'sort 
+        'horzontal
+        'check ratio
+        'devide
+        Dim i As Integer
+        Dim Y As Integer
+        Dim prevX As Integer = 0
+        Dim prevY As Integer = 0
+        Dim tempTotalX As Double
+
+        'On Error Resume Next
+        totalWidth = 0
+        totalHeight = 0
+        tempTotalX = 0
+        prevY = 0
+
+        Y = 0
+        lstcordX.Items.Clear()
+        lstcordY.Items.Clear()
+        Logging("Possible total width =" + CStr(stopWidth))
+        For i = 0 To lstNodeWidth.Items.Count - 1
+            Application.DoEvents()
+            If (tempTotalX + CDbl(lstNodeWidth.Items(i)) <= stopWidth Or tempTotalX + CDbl(lstNodeWidth.Items(i)) <= totalWidth) Then
+                If i = 0 Then
+                    prevY = i
+                    'First Coordinate
+                    lstcordX.Items.Add(0)
+                    lstcordY.Items.Add(Y)
+                    tempTotalX = tempTotalX + lstNodeWidth.Items(i) 'Use to calculate longest X
+                    totalHeight = totalHeight + lstNodeHeight.Items(i)
+
+                Else
+                    prevX = i - 1
+                    lstcordX.Items.Add(CStr(CInt(lstNodeWidth.Items(prevX)) + CInt(lstcordX.Items(prevX))))
+                    lstcordY.Items.Add(Y)
+                    tempTotalX = tempTotalX + lstNodeWidth.Items(i) 'Use to calculate longest X
+
+                End If
+            Else
+                Y = Y + CInt(lstNodeHeight.Items(prevY))
+                prevY = i
+                totalHeight = totalHeight + lstNodeHeight.Items(prevY)
+                If tempTotalX > totalWidth Then totalWidth = tempTotalX
+                tempTotalX = 0
+                lstcordX.Items.Add(0)
+                lstcordY.Items.Add(Y)
+                tempTotalX = tempTotalX + lstNodeWidth.Items(i) 'Use to calculate longest X
+            End If
+        Next
+
+        RefreshFloorPlan()
+
+        tArea = CDbl(totalHeight) * CDbl(totalWidth)
+        tArea = tArea * Scales * Scales
+
+        Logging("Total Width  : " & CStr(CLng(totalWidth * Scales)))
+        Logging("Total Height : " & CStr(CLng(totalHeight * Scales)))
+        Logging("Height/Width : " & CStr((totalHeight / totalWidth)))
+        Logging("Width/Height : " & CStr((totalWidth / totalHeight)))
+        Logging("Area        :  " & CLng(tArea))
+        Logging("###############################")
+    End Sub
+    Public Sub RefreshFloorPlan()
+        picFloorPlan.Image = Nothing
+        picFloorPlan.Refresh()
+
+        For i = 0 To lstNodeWidth.Items.Count - 1
+            DrawRectangle(CInt(lstcordX.Items(i)), CInt(lstcordY.Items(i)), CInt(lstNodeWidth.Items(i)), CInt(lstNodeHeight.Items(i)))
+        Next
+
+    End Sub
+    Private Sub SaveFile()
+
+        Dim FileName As String
+        Dim StrArray As New System.Text.StringBuilder()
+        Dim StrHeader As String = IIf(Scales > 1, "Floorplan Scaled for " + CStr(Scales) + " smaller" + vbCr + vbLf, "") + "Node name" + vbTab + "Width" + vbTab + "Height" + vbTab + "X" + vbTab + "Y"
+
+        Dim i As Integer
+
+        FileName = ""
+        cmOpendlg.FileName = ""
+        cmOpendlg.ShowDialog()
+        FileName = cmOpendlg.FileName
+
+        If FileName = "" Then Exit Sub
+        MsgBox(FileName)
+        StrArray.Append(StrHeader)
+
+        If lstcordX.Items.Count = 0 Then Exit Sub
+
+        For i = 0 To lstNodeName.Items.Count - 1
+            StrArray.Append(vbCr + vbLf + CStr(lstNodeName.Items(i)) + vbTab + CStr(CInt(lstNodeWidth.Items(i))) + vbTab + CStr(CInt(lstNodeHeight.Items(i))) + vbTab + CStr(lstcordX.Items(i)) + vbTab + CStr(lstcordY.Items(i)))
+        Next
+        System.IO.File.WriteAllText(FileName, StrArray.ToString())
+    End Sub
+
+    Private Sub Loadfile()
+        Dim ReadSplit() As String
+        Dim FileName As String
+        Dim i As Integer
+        FileName = ""
+        InitialArea = 0
+        cmdlg.FileName = ""
+        cmdlg.ShowDialog()
+        FileName = cmdlg.FileName
+
+        If (FileName = "") Then Exit Sub
+        'FileName = "C:\Users\boonkhai\Desktop\adaptec1\adaptec.txt"
+        Logging("Load file : " & FileName)
+        Dim lines() As String = IO.File.ReadAllLines(FileName)
+        'On Error GoTo Err
+        lstLoad.Items.AddRange(lines)
+        lstLoad.Items.Remove("")
+        For i = 0 To lstLoad.Items.Count - 1
+
+            If (CStr(lstLoad.Items(i))).Substring(0, 1) = vbTab Then
+                If CStr(lstLoad.Items(i)).Length > 20 Then
+                    If CStr(lstLoad.Items(i)).Substring(CStr(lstLoad.Items(i)).Length - 8, 8) = "terminal" Then
+                        ReadSplit = Split((CStr(lstLoad.Items(i))), vbTab)
+                        lstNodeName.Items.Add(ReadSplit(1))
+                        lstUnscaledNodeWidth.Items.Add(CInt(ReadSplit(2)))
+                        lstUnscaledNodeHeight.Items.Add(CInt(ReadSplit(3)))
+                        lstNodeWidth.Items.Add(CInt(ReadSplit(2)) / Scales)
+                        lstNodeHeight.Items.Add(CInt(ReadSplit(3)) / Scales)
+                        InitialArea = InitialArea + (CDbl(ReadSplit(2)) * CDbl(ReadSplit(3)))
+                    End If
+                End If
+            End If
+        Next
+        Logging("Estimated Initial Area: " + CStr(InitialArea))
+        Sort()
+        MsgBox("Done Loading", vbYes + vbInformation, "Done")
+
+        Exit Sub
+Err:
+        MsgBox("Error Readig file", vbExclamation, "Error")
+
+    End Sub
+    Public Function RoundUp(input As Double) As Integer
+        RoundUp = IIf(input > CInt(input), CInt(input) + 1, CInt(input))
+    End Function
+    Private Sub Sort()
+        'Sort Height
+        Logging("Sorting the height")
+        Dim i As Integer
+        Dim j As Integer
+        Dim MaxLoc As Integer
+        Dim maxCount As Integer
+        Dim Max As Integer = 0
+        maxCount = lstNodeHeight.Items.Count - 1
+        For j = 0 To maxCount
+            For i = 0 To lstNodeHeight.Items.Count - 1
+                If Max < CInt(lstNodeHeight.Items(i)) Then Max = CInt(lstNodeHeight.Items(i)) : MaxLoc = i
+            Next i
+
+            lstNodeNameSort.Items.Add(lstNodeName.Items(MaxLoc))
+            lstNodeWidthSort.Items.Add(lstNodeWidth.Items(MaxLoc))
+            lstNodeHeightSort.Items.Add(lstNodeHeight.Items(MaxLoc))
+
+            lstNodeHeight.Items.RemoveAt(MaxLoc)
+            lstNodeWidth.Items.RemoveAt(MaxLoc)
+            lstNodeName.Items.RemoveAt(MaxLoc)
+            Max = 0
+            MaxLoc = 0
+        Next j
+
+        For i = 0 To lstNodeNameSort.Items.Count - 1
+            lstNodeName.Items.Add(lstNodeNameSort.Items(i))
+            lstNodeWidth.Items.Add(lstNodeWidthSort.Items(i))
+            lstNodeHeight.Items.Add(lstNodeHeightSort.Items(i))
+        Next
+        lstNodeHeightSort.Items.Clear()
+        lstNodeWidthSort.Items.Clear()
+        lstNodeNameSort.Items.Clear()
+        Logging("Sorted")
+    End Sub
+    Private Sub DrawRectangle(posX As Integer, posY As Integer, tWidth As Integer, tHeight As Integer)
+        Dim myGraphic As Graphics
+        Dim myRectangle As Rectangle
+        Dim myFrame As Rectangle
+        Dim myBorder As Rectangle
+        Dim myFloorPlan As Rectangle
+
+
+        Dim myPen As New Pen(Color.Black)
+        Dim myBrush As New SolidBrush(Color.Blue)
+        Dim myFloorPlanColor As New Pen(Color.Green)
+
+        posY = picFloorPlan.Height - posY - tHeight  'Calibrate left bottom as origin
+
+        myGraphic = Graphics.FromHwnd(picFloorPlan.Handle)
+
+        myFrame = New Rectangle(x:=0, y:=0, width:=picFloorPlan.Width - 1, height:=picFloorPlan.Height - 1)
+        myRectangle = New Rectangle(x:=posX, y:=posY, width:=tWidth, height:=tHeight)
+        myBorder = New Rectangle(x:=posX, y:=posY, width:=tWidth, height:=tHeight)
+        myFloorPlan = New Rectangle(x:=0, y:=picFloorPlan.Height - RoundUp(totalHeight), width:=RoundUp(totalWidth), height:=RoundUp(totalHeight))
+
+        myGraphic.FillRectangle(myBrush, myRectangle)
+        myGraphic.DrawRectangle(pen:=myPen, rect:=myBorder)
+        myGraphic.DrawRectangle(pen:=myPen, rect:=myFrame)
+        myGraphic.DrawRectangle(pen:=myFloorPlanColor, rect:=myFloorPlan)
+    End Sub
+    Private Sub cmdPlacement_Click(sender As Object, e As EventArgs) Handles cmdPlacement.Click
+        Logging("Start Placing...")
+        Dim PossibleArea As Double
+        Dim PossibletotalWidth As Double
+
+        'Get estimated Posible Area'
+        PossibleArea = InitialArea * 10 / (Scales * Scales)
+        PossibletotalWidth = PossibleArea ^ (1 / 2)
+
+        While (DesignRuleCheck(totalHeight, totalWidth) = False And PossibletotalWidth > 10)
+            PossibletotalWidth = PossibletotalWidth / 1.2
+            Placement(PossibletotalWidth)
+        End While
+
+        Logging("Done Placing...")
+
+        MsgBox("Total Width  : " & CStr(CLng(totalWidth * Scales)) & vbCr + vbLf &
+               "Total Height : " & CStr(CLng(totalHeight * Scales)) & vbCr + vbLf &
+               "Height/Width : " & CStr((totalHeight / totalWidth)) & vbCr + vbLf &
+               "Width/Height : " & CStr((totalWidth / totalHeight)) & vbCr + vbLf &
+               "Area        :  " & CStr(CLng(tArea)), vbInformation, "Result")
+
+    End Sub
+    Private Sub cmdLoad_Click(sender As Object, e As EventArgs) Handles cmdLoad.Click
+        Dim msgAns As MessageBoxOptions
+        If FirstimeRun = False Then
+            msgAns = MsgBox("Are you sure? All result will be cleared", vbYesNo + vbInformation, "Load File?")
+        End If
+
+        If msgAns = vbNo Then Exit Sub
+        Scales = CInt(cboScale.Text)
+        If Scales <= 0 Then MsgBox("Scales must be positive integer") : Exit Sub
+        lstlog.Items.Clear()
+        txtLog.Text = ""
+        Initialize()
+
+            Loadfile()
+
+    End Sub
+    Private Sub Logging(tdata As String)
+        lstlog.Items.Add(tdata)
+        lstlog.SelectedIndex = lstlog.Items.Count - 1
+        txtLog.AppendText(tdata + vbCr + vbLf)
+
+    End Sub
+    Public Sub Initialize()
+        totalWidth = 0
+        totalHeight = 0
+
+        lstNodeHeight.Items.Clear()
+        lstNodeHeightSort.Items.Clear()
+        lstNodeWidth.Items.Clear()
+        lstNodeWidthSort.Items.Clear()
+        lstNodeName.Items.Clear()
+        lstNodeName.Items.Clear()
+        lstcordX.Items.Clear()
+        lstcordY.Items.Clear()
+        lstLoad.Items.Clear()
+
+        picFloorPlan.Image = Nothing
+
+    End Sub
+    Private Sub frmmain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Initialize()
+        cboScale.Text = "30"
+        For i = 1 To 10
+            cboScale.Items.Add(i * 10)
+        Next
+
+    End Sub
+
+    Private Sub cmdRerun_Click(sender As Object, e As EventArgs) Handles cmdRerun.Click
+        Dim stopWidth As Double
+        stopWidth = CDbl(InputBox("Please insert width"))
+        Placement(stopWidth)
+
+        MsgBox("Total Width  : " & CStr(CLng(totalWidth * Scales)) & vbCr + vbLf &
+               "Total Height : " & CStr(CLng(totalHeight * Scales)) & vbCr + vbLf &
+               "Height/Width : " & CStr((totalHeight / totalWidth)) & vbCr + vbLf &
+               "Width/Height : " & CStr((totalWidth / totalHeight)) & vbCr + vbLf &
+               "Area        :  " & CStr(CLng(tArea)), vbInformation, "Result")
+    End Sub
+
+    Private Sub cmdExport_Click(sender As Object, e As EventArgs) Handles cmdExport.Click
+        SaveFile()
+    End Sub
+End Class
